f4189f57d59b39eba228e1a59444f8c2d945e29d
Ben Poole <ben.poole@aerian.com>
2016-08-11 15:38:22 +0100

    Full path to PHP required when executed by cron

--- cron/merlin-sync.sh
+++ cron/merlin-sync.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Import the last 12 month one by one
-php ../public/index.php import --month=1
-php ../public/index.php import --month=2
-php ../public/index.php import --month=3
-php ../public/index.php import --month=4
-php ../public/index.php import --month=5
-php ../public/index.php import --month=6
-php ../public/index.php import --month=7
-php ../public/index.php import --month=8
-php ../public/index.php import --month=9
-php ../public/index.php import --month=10
-php ../public/index.php import --month=11
-php ../public/index.php import --month=12
+/usr/bin/php ../public/index.php import --month=1
+/usr/bin/php ../public/index.php import --month=2
+/usr/bin/php ../public/index.php import --month=3
+/usr/bin/php ../public/index.php import --month=4
+/usr/bin/php ../public/index.php import --month=5
+/usr/bin/php ../public/index.php import --month=6
+/usr/bin/php ../public/index.php import --month=7
+/usr/bin/php ../public/index.php import --month=8
+/usr/bin/php ../public/index.php import --month=9
+/usr/bin/php ../public/index.php import --month=10
+/usr/bin/php ../public/index.php import --month=11
+/usr/bin/php ../public/index.php import --month=12
 
 # Delete too old data and delete profile
-php ../public/index.php delete
+/usr/bin/php ../public/index.php delete
 
