269283fbee23f26c76fd9f142c7a6eb669e1b41d
Ludovic Gouzille <ludovic.gouzille@aerian.com>
2016-09-22 16:28:03 +0100

    Deleting unnecessary files

--- module/Common/Module.php
+++ module/Common/Module.php
@@ -1,3 +1,31 @@
 <?php
 
-require __DIR__ . '/src/Module.php';
+namespace Common;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements ConfigProviderInterface, AutoloaderProviderInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfig()
+    {
+        return require __DIR__ . '/config/module.config.php';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAutoloaderConfig()
+    {
+        return [
+            'Zend\Loader\StandardAutoloader' => [
+                'namespaces' => [
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ],
+            ],
+        ];
+    }
+}
--- module/Console/Module.php
+++ module/Console/Module.php
@@ -1,3 +1,26 @@
 <?php
 
-require __DIR__ . '/src/Module.php';
+namespace Console;
+
+use Zend\Console\Adapter\AdapterInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\ConsoleUsageProviderInterface;
+
+class Module implements ConfigProviderInterface, ConsoleUsageProviderInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfig()
+    {
+        return require __DIR__ . '/config/module.config.php';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getConsoleUsage(AdapterInterface $console)
+    {
+        return require __DIR__ . '/config/console-usage.config.php';
+    }
+}
