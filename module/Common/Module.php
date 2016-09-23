8f395e6e09364a6cf8bd4b28421f2473941fbb2d
Ludovic Gouzille <ludovic.gouzille@aerian.com>
2016-09-23 16:13:24 +0100

    Adding bootstrap to common module

--- module/Common/Module.php
+++ module/Common/Module.php
@@ -2,10 +2,15 @@
 
 namespace Common;
 
+use Zend\EventManager\EventInterface;
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\BootstrapListenerInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+use Zend\View\Model\JsonModel;
 
-class Module implements ConfigProviderInterface, AutoloaderProviderInterface
+class Module implements ConfigProviderInterface, AutoloaderProviderInterface, BootstrapListenerInterface
 {
     /**
      * {@inheritDoc}
@@ -27,5 +32,81 @@ class Module implements ConfigProviderInterface, AutoloaderProviderInterface
                 ],
             ],
         ];
+    }
+
+    /**
+     * @param EventInterface $e
+     *
+     * @return null
+     */
+    public function onBootstrap(EventInterface $e)
+    {
+        /** @var MvcEvent $e */
+        $eventManager = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+
+        $eventManager->attach(MvcEvent::EVENT_DISPATCH_ERROR, array($this, 'onDispatchError'), 0);
+        $eventManager->attach(MvcEvent::EVENT_RENDER_ERROR, array($this, 'onRenderError'), 0);
+    }
+
+    /**
+     * @param MvcEvent $e
+     *
+     * @return null|JsonModel
+     */
+    public function onDispatchError(MvcEvent $e)
+    {
+        return $this->getJsonModelError($e);
+    }
+
+    /**
+     * @param MvcEvent $e
+     *
+     * @return JsonModel
+     */
+    public function onRenderError(MvcEvent $e)
+    {
+        return $this->getJsonModelError($e);
+    }
+
+    /**
+     * @param MvcEvent $e
+     *
+     * @return JsonModel
+     */
+    public function getJsonModelError(MvcEvent $e)
+    {
+        $error = $e->getError();
+        if (!$error) {
+            return new JsonModel();
+        }
+
+        $exception = $e->getParam('exception');
+        $exceptionJson = array();
+        if ($exception) {
+            $exceptionJson = array(
+                'class' => get_class($exception),
+                'file' => $exception->getFile(),
+                'line' => $exception->getLine(),
+                'message' => $exception->getMessage(),
+                'stacktrace' => $exception->getTraceAsString()
+            );
+        }
+
+        $errorJson = array(
+            'message'   => 'An error occurred during execution; please try again later.',
+            'error'     => $error,
+            'exception' => $exceptionJson,
+        );
+        if ($error == 'error-router-no-match') {
+            $errorJson['message'] = 'Resource not found.';
+        }
+
+        $model = new JsonModel(array('errors' => array($errorJson)));
+
+        $e->setResult($model);
+
+        return $model;
     }
 }
