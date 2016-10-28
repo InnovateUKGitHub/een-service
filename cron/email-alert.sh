7b57d605524e1286f66fe91fac7f69c5f7d39204
Ludovic Gouzille <ludovic.gouzille@aerian.com>
2016-10-28 11:55:57 +0100

    EEN:141 Adding email alert daily cronjob

--- cron/email-alert.sh
+++ cron/email-alert.sh
@@ -3,5 +3,6 @@
 # Send email alert to user - spike temporary for Methods development
 # Replace CONTACT by the id of a contact in salesforce
 # And repeat the line for as many user as you want
-/usr/bin/php ../public/index.php email-alert --user=CONTACT
+/usr/bin/php ../public/index.php email-alert --user=0038E00000RnHs4
+/usr/bin/php ../public/index.php email-alert --user=0038E00000RnHu0
 
